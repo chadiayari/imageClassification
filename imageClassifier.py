5decbe063a4f4cedb2f1c572eb389000defbf413
Chadi Ayari <40720094+chadiayari@users.noreply.github.com>
2021-12-06 16:27:48 +0100

    Added CNN classification
    CNN classification made accuracy results better, compared to ANN classification.

--- imageClassifier.py
+++ imageClassifier.py
@@ -41,3 +41,46 @@ ann = models.Sequential([
 ann.compile(optimizer='SGD', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 ann.fit(X_train, y_train, epochs=5)
+
+#printing accuracy report for ANN classification
+from sklearn.metrics import confusion_matrix , classification_report
+import numpy as np
+
+y_pred = ann.predict(X_test)
+y_pred_classes = [np.argmax(element) for element in y_pred]
+
+print("Classification Report: \n", classification_report(y_test, y_pred_classes))
+
+#Using CNN for classification
+cnn = models.Sequential([
+    layers.Conv2D(filters=32, kernel_size=(3, 3), activation='relu', input_shape=(32, 32, 3)),
+    layers.MaxPooling2D((2, 2)),
+    
+    layers.Conv2D(filters=64, kernel_size=(3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+
+cnn.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+cnn.fit(X_train, y_train, epochs=10)
+#gives better accuracy: (0.7938)
+
+cnn.evaluate(X_test,y_test)
+#accuracy goes down to 0.6982
+
+y_pred = cnn.predict(X_test)
+y_pred[:5]
+
+y_classes = [np.argmax(element) for element in y_pred]
+y_classes[:5]
+
+y_test[:5]
+
+plot_sample(X_test, y_test,9999)
+#displays the image of a horse
+
+classes[y_classes[200]]
+#displays 'dog'
